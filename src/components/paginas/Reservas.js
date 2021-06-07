376bd7432be905779b8975e78036a1ad5d29918b
gejurier <julianariera1998@gmail.com>
2021-06-06 21:18:05 -0500

    panel admins avanzando... christian y genesis

---
+++ src/components/paginas/Reservas.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reservas = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-light mb-4">Reservas</h1>
+        </>
+    );
+}
+
+export default Reservas;
